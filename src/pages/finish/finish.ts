1ef500214607bf78e6c22ef2c8c8a63f4d131bb8
Cristóbal Domínguez <cristobaldominguez95@gmail.com>
2018-02-04 00:46:07 +0100

    Create 'pending' and finish 'pages' and add them to tabs

---
+++ src/pages/finish/finish.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-finish',
+  templateUrl: 'finish.html'
+})
+export class FinishPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
---
+++ src/pages/pending/pending.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-pending',
+  templateUrl: 'pending.html'
+})
+export class PendingPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
